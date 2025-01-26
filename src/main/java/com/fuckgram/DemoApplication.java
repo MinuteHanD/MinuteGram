c364da3f726cbf573047f17d2f4b505521cf35d6
Amritanshu Darbari <amritanshu985@gmail.com>
2025-01-27 01:40:10 +0530

    fix: working/testing deployment. resorting to reading env variables from system instead of .env

--- src/main/java/com/fuckgram/DemoApplication.java
+++ src/main/java/com/fuckgram/DemoApplication.java
@@ -12,23 +12,22 @@ import org.springframework.context.annotation.Configuration;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().directory("../").load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-		String dbUrl = dotenv.get("DB_URL", System.getenv("DB_URL"));
-		String dbUsername = dotenv.get("DB_USERNAME", System.getenv("DB_USERNAME"));
-		String dbPassword = dotenv.get("DB_PASSWORD", System.getenv("DB_PASSWORD"));
-		String jwtSecret = dotenv.get("JWT_SECRET", System.getenv("JWT_SECRET"));
-		String serverPort = dotenv.get("SERVER_PORT", System.getenv("SERVER_PORT"));
-		String jwtExpiration = dotenv.get("JWT_EXPIRATION", System.getenv("JWT_EXPIRATION"));
+		//Dotenv dotenv = Dotenv.configure().ignoreIfMissing().directory("../").load();
+		//dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		String dbUrl = System.getenv("DB_URL");
+		String dbUsername = System.getenv("DB_USERNAME");
+		String dbPassword = System.getenv("DB_PASSWORD");
+		String jwtSecret = System.getenv("JWT_SECRET");
+		String serverPort = System.getenv("SERVER_PORT");
+		String jwtExpiration = System.getenv("JWT_EXPIRATION");
 
-// Set system properties
+		// Set system properties
 		System.setProperty("spring.datasource.url", dbUrl);
 		System.setProperty("spring.datasource.username", dbUsername);
 		System.setProperty("spring.datasource.password", dbPassword);
 		System.setProperty("jwt.secret", jwtSecret);
 		System.setProperty("server.port", serverPort);
 		System.setProperty("jwt.expiration", jwtExpiration);
-		SpringApplication.run(DemoApplication.class, args);
 
 	}
 
