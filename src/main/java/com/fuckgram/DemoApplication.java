c8072657027a18c9f9955580478020cd951694da
Amritanshu Darbari <amritanshu985@gmail.com>
2025-01-26 22:52:52 +0530

    feat: configure .jar file to read .env file

--- src/main/java/com/fuckgram/DemoApplication.java
+++ src/main/java/com/fuckgram/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.fuckgram;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,6 +12,8 @@ import org.springframework.context.annotation.Configuration;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("../").load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
