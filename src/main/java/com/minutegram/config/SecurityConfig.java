0a11614882babbee32e04673f14170c01fcd5eef
Amritanshu Darbari <121439931+MinuteHanD@users.noreply.github.com>
2025-09-29 15:29:41 +0530

    new client endpoint

--- src/main/java/com/minutegram/config/SecurityConfig.java
+++ src/main/java/com/minutegram/config/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://regal-selkie-67277e.netlify.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://minutegram.netlify.app"));
 
         configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
